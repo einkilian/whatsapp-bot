cf79c958f02491fe2700d405e1e552a7ecc7f58f
Kilian <main.einkilian@gmail.com>
2025-10-15 13:47:57 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import pkg from "whatsapp-web.js";
 const { Client, LocalAuth } = pkg;
 import qrcode from "qrcode-terminal";
-import axios from "axios";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
 dotenv.config();
@@ -41,7 +40,16 @@ client.on("message", async msg => {
         const body = msg.body && msg.body.toLowerCase();
 
         console.log(`📩 Nachricht von ${from}: ${msg.body} (ID: ${msg.id._serialized})`);
+        console.log("Vollständige Nachricht:", msg);
+        
+        // Nachricht als gelesen markieren
         await client.sendSeen(from);
+        // Chat-Info an Backend senden
+        await fetch(`http://192.168.250.1:5678/webhook-test/updatechat`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ id: msg.id._serialized, name: msg._data.notifyName }),
+        });
 
         // Beispiel: wenn Nachricht mit "/echo " beginnt, dann echo
         if (body && body.startsWith("/echo ")) {
@@ -50,28 +58,64 @@ client.on("message", async msg => {
             return;
         }
 
+        // All regualr commands send to n8n backend
         if (body && body.startsWith("/")) {
-            // Beispiel: /"part1" "part2" "content"
-            const match = msg.body.match(/^\/"([^"]+)"\s+"([^"]+)"\s+"([^"]+)"$/);
+            const match = msg.body.match(/^\/(?:"([^\"]+)"\s+"([^\"]+)"\s+"([^\"]+)")$/) || msg.body.match(/^\/"([^\"]+)"\s+"([^\"]+)"\s+"([^\"]+)"$/);
             if (match) {
                 const [, part1, part2, content] = match;
                 console.log(`part1: ${part1}, part2: ${part2}, content: ${content}`);
+
                 try {
-                    await fetch(`http://192.168.250.1:5678/webhook-test/${part1}/${part2}`, {
+                    // include '=' and encode parts
+                    const response = await fetch(`http://192.168.250.1:5678/webhook-test/checkPermission?cmd=${encodeURIComponent(part1 + '-' + part2)}`, {
                         method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({ content }),
                     });
-                    await client.sendMessage(from, `Webhook call to /${part1}/${part2} successful.`);
+                    if (response.status === 200) {
+                        try {
+                            await fetch(`http://192.168.250.1:5678/webhook-test/${part1}/${part2}`, {
+                                method: "POST",
+                                headers: { "Content-Type": "application/json" },
+                                body: JSON.stringify({ content: content, chatId: from, messageId: msg.id._serialized}),
+                            });
+                            await client.sendMessage(from, `Webhook call to /${part1}/${part2} successful.`);
+                        } catch (err) {
+                            console.error("Webhook error:", err.message);
+                            await client.sendMessage(from, `Dieser Command existiert nicht.`);
+                        }
+                    } else if (response.status === 403) {
+                        await msg.reply("Für diesen Command hast du keine Berechtigung.");
+                        return;
+                    } else {
+                        return;
+                    }
+
                 } catch (err) {
                     console.error("Webhook error:", err.message);
-                    await client.sendMessage(from, `Webhook call failed: ${err.message}`);
                 }
+
+
                 return;
             }
         }
 
-
+        /*if (body && body.startsWith("/verify ")) {
+            try {
+                const response = await fetch("http://192.168.250.1:5678/webhook/wab/grand", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ from, message: msg.body }),
+                });
+                if (response.ok) {
+                    await client.sendMessage(from, "Webhook /grand erfolgreich aufgerufen.");
+                } else {
+                    await client.sendMessage(from, "Fehler beim Aufruf des /grand Webhooks.");
+                }
+            } catch (err) {
+                console.error("Webhook /grand error:", err.message);
+                await client.sendMessage(from, "Fehler beim Aufruf des /grand Webhooks.");
+            }
+            return;
+        }*/
     } catch (e) {
         console.error("Fehler im message handler:", e);
     }
@@ -133,7 +177,7 @@ app.post("/send", async (req, res) => {
 
 });
 app.post("/react", async (req, res) => {
-    const { messageId, emoji } = req.body;
+    const { messageId, emoji } = req.query;
     if (!messageId || !emoji) {
         return res.status(400).json({ error: "messageId und emoji erforderlich" });
     }
